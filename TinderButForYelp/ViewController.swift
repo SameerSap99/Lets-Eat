fff6af1164a1fc29c00a5662ae42a801678a4984
Sameer Saptarshi <sameer.saptarshi@berkeley.edu>
2020-07-23 23:28:18 -0700

    Initial Commit

---
+++ TinderButForYelp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderButForYelp
+//
+//  Created by Sameer Saptarshi on 7/23/20.
+//  Copyright © 2020 SameerSaptarshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
