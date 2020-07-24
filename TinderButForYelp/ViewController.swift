6b2bbdcc02bdb92edfadd07c0e0428741faac00d
Sameer Saptarshi <sameer.saptarshi@berkeley.edu>
2020-07-24 00:43:49 -0700

    Initial swiping functionality

--- TinderButForYelp/ViewController.swift
+++ TinderButForYelp/ViewController.swift
@@ -14,7 +14,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet var card: UIView!
+    
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
 
-
+        if sender.state == UIGestureRecognizer.State.ended {
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y)
+                })
+                return
+            } else if card.center.x > (view.frame.width - 75) {
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y)
+                })
+                return
+            }
+            UIView.animate(withDuration: 0.2, animations: {
+                card.center = self.view.center
+            })
+        }
+        
+    }
+    
 }
 
